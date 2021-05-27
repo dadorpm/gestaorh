42525c35ce23bec85bcab105b034a3ba52cc5761
adauto meireles dibay <dadorpm@hotmail.com>
2021-05-27 19:35:07 -0300

    relacionamentos

--- empresa/models.py
+++ empresa/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=150, help_text='nome da empresa')
+
+    def __str__(self):
+        return self.nome
--- funcionario/models.py
+++ funcionario/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from setor.models import Setor
+from empresa.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    setor = models.ManyToManyField(Setor)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.nome
