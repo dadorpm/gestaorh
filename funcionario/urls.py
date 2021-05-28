47ded1ec039a710b3a336221186290ce4c9d57f7
adauto meireles dibay <dadorpm@hotmail.com>
2021-05-28 04:59:41 -0300

    deletar usuarios

--- funcionario/urls.py
+++ funcionario/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FuncionarioListView, FuncionarioEditView
+from .views import FuncionarioListView, FuncionarioEditView, FuncionarioDeleteView
 
 urlpatterns = [
     path('', FuncionarioListView.as_view(), name='lista_funcionarios'),
     path('edita/<int:pk>/', FuncionarioEditView.as_view(), name='edita_funcionario'),
+    path('apaga/<int:pk>/', FuncionarioDeleteView.as_view(), name='apaga_funcionario'),
 ]
--- funcionario/views.py
+++ funcionario/views.py
@@ -1,6 +1,9 @@
+from django.urls import reverse_lazy
+
 from .models import Funcionario
 from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView
+
 
 # Create your views here.
 class FuncionarioListView(ListView):
@@ -12,4 +15,6 @@ class FuncionarioListView(ListView):
 class FuncionarioEditView(UpdateView):
     model = Funcionario
     fields = ['nome', 'setor']
-
+class FuncionarioDeleteView(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('lista_funcionarios')
